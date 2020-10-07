d3b5dc47c25a5313b645ab62a42b7ae5c15e1b6a
smntin <smn.pankv@gmail.com>
2020-10-07 17:28:55 +0300

    Improve exceptions

--- src/main/kotlin/ru/emkn/virtualmemory/analysis/cli/Parsers.kt
+++ src/main/kotlin/ru/emkn/virtualmemory/analysis/cli/Parsers.kt
@@ -3,6 +3,11 @@ package ru.emkn.virtualmemory.analysis.cli
 import ru.emkn.virtualmemory.algorithms.Page
 import ru.emkn.virtualmemory.cli.ParseException
 
+/**
+ * Parses pages indices from input string and converts
+ * them to Pages with respective indices.
+ * @throws ParseException
+ */
 fun parseListOfPages(str: String): List<Page> {
     try {
         val parsed = str
@@ -12,12 +17,13 @@ fun parseListOfPages(str: String): List<Page> {
             if (page.index < 0)
                 throw ParseException("Page index must be positive.")
         return parsed
-    } catch (e: ParseException) {
-        throw e
-    } catch (e: Throwable) {
-        throw ParseException("Failed to parse input file.")
+    } catch (e: NumberFormatException) {
+        throw ParseException("Failed to parse one of the numbers in the input file: " + e.message)
     }
 }
 
+/**
+ * @throws ParseException
+ */
 fun parseSeveralListsOfPages(lines: List<String>): List<List<Page>> =
     lines.map { line -> parseListOfPages(line) }
